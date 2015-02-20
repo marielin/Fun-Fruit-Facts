2c79edd180ff532258a01b57ec266de77cc337d2
Marie Lin <marilinsapphire@gmail.com>
2015-02-20 06:10:13 +0000

    final draft

--- js/script.js
+++ js/script.js
@@ -3,48 +3,72 @@ $(document).ready(function () {
 })
 
 function initCarousel() {
-	var panes = $(".pane");
+	var panes = $(".panes").children();
+	var numPanes = panes.length;
 	var curPane = 0;
-	var duration = 4000;
+	var duration = 6000;
 	var timer = setInterval(function(){autoAdvance()}, duration);
 
 	$("#prev").click(function(){rewind()});
 	$("#next").click(function(){advance()});
 
-	panes.eq((curPane + 1) % panes.length).addClass("hidden");
-	panes.eq((curPane + 2) % panes.length).addClass("hidden");
-
 	function autoAdvance() {
-		panes.eq((curPane + 0) % panes.length).addClass("hidden");
-		panes.eq((curPane + 1) % panes.length).removeClass("hidden");
-		curPane = (curPane + 1) % panes.length;
+		curPane = (curPane + 1) % numPanes;
+		for(i = 0; i < numPanes; i++) {
+			if (i == curPane) panes.eq(i).children("img").removeClass("hidden");
+			else panes.eq(i).children("img").addClass("hidden");
+		}
 	}
 
 	function advance() {
 		clearInterval(timer);
-		panes.eq((curPane + 0) % panes.length).addClass("hidden");
-		panes.eq((curPane + 1) % panes.length).removeClass("hidden");
-		curPane = (curPane + 1) % panes.length;
+		curPane = (curPane + 1) % numPanes;
+		for(i = 0; i < numPanes; i++) {
+			if (i == curPane) panes.eq(i).children("img").removeClass("hidden");
+			else panes.eq(i).children("img").addClass("hidden");
+		}
 		timer = setInterval(function(){autoAdvance()}, duration);
 	}
 
 	function rewind() {
 		clearInterval(timer);
-		panes.eq((curPane + 0) % panes.length).addClass("hidden");
-		panes.eq((curPane + 1) % panes.length).removeClass("hidden");
-		curPane = (curPane + 1) % panes.length;
+		curPane = (curPane + 1) % numPanes;
+		for(i = 0; i < numPanes; i++) {
+			if (i == curPane) panes.eq(i).children("img").removeClass("hidden");
+			else panes.eq(i).children("img").addClass("hidden");
+		}
 		timer = setInterval(function(){autoAdvance()}, duration);
 	}
 }
 
 $(window).scroll(function() {
-	var threshold = 300;
+	var shrinkThreshold = 300;
 	var scrollpos = window.pageYOffset || document.documentElement.scrollTop;
 
-	if (scrollpos >= threshold)
+	if (scrollpos >= shrinkThreshold)
 		$("header").addClass("shrunk");
 	else
 		$("header").removeClass("shrunk");
+
+	var links = $("header nav").children("a");
+	var sections = $("main").children("section");
+	var numSections = sections.length;
+
+	if (scrollpos >= $(document).height() - $(window).height()) {
+		$("header nav a").removeClass("currentSection");
+		links.eq(numSections - 1).addClass("currentSection");
+	} else {
+		for (i = 0; i < numSections - 1; i++) {
+			if (scrollpos >= sections.eq(i).offset().top - 60) {
+				$("header nav a").removeClass("currentSection");
+				links.eq(i).addClass("currentSection");
+			}
+		}
+	}
+
+	if (scrollpos < sections.eq(0).offset().top - 60) {
+		$("header nav a").removeClass("currentSection");
+	}
 })
 
 $("#button").click(function() {
@@ -52,3 +76,15 @@ $("#button").click(function() {
         scrollTop: $("#myDiv").offset().top
     }, 2000);
 });
+
+$("header nav a").click(function(){
+	$('html, body').animate({
+		scrollTop: $("#" + $(this).text()).offset().top - 60
+	}, 1000, 'easeInOutCubic');
+})
+
+$("header h1").click(function(){
+	$('html, body').animate({
+		scrollTop: 0
+	}, 1000, 'easeInOutCubic');
+})
